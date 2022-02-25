6a3a8833af2bb1b2352f1e0423ddf8d6c7ccd1a0
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2022-02-25 15:21:26 -0300

    25-02-2022 Creacion del codigo del controlador para mostrar la informacion de la bd, cambio de los nombres de los archivos del controller y del model

---
+++ controller/producto.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once("../config/conexion.php");
+require_once("../models/Producto.php");
+
+//instanciando un ubjeto de la clase Productos
+$producto = new Producto();
+
+switch ($_GET['op']) {
+    case 'listar':
+        $datos = $producto->get_produto();
+        $data = Array();
+        foreach ($datos as $row) {
+            $sub_array = array();
+            $sub_array[] = $row['prod_nom'];
+            $sub_array[] = '<button type="button" onClick="editar('.$row["prod_id"].');" id="'.$row["prod_id"].'" class="btn btn-outline-primary btn-icon"><div><i class="fa fa-edit"></i></div></button>';
+            $sub_array[] = '<button type="button" onClick="eliminar('.$row["prod_id"].');" id="'.$row["prod_id"].'" class="btn btn-outline-danger btn-icon"><div><i class="fa fa-trash"></i></div></button>';
+        }
+
+        $results = array(
+            "sEcho"=>1,
+            "iTotalRecords"=>count($data),
+            "iTotalDisplayRecords"=>count($data),
+            "aaData"=>$data);
+
+        echo json_encode($results);
+
+    break;
+
+}
+?>
