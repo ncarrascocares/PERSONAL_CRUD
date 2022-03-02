4e7822cc964ff168d7395818d9a9ba4f7b91df23
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2022-03-01 23:51:39 -0300

    solo modificación de la estructura de las palabras

--- models/Producto.php
+++ models/Producto.php
@@ -3,7 +3,7 @@
     class Producto extends Conectar{
 
         //funcion para obtener los productos desde la bd
-        public function get_produto(){
+        public function get_producto(){
 
             //Creacion de variables que extienden desde la clase Conectar
             $conectar = parent::conexion();
@@ -11,14 +11,14 @@
             $sql = "SELECT * FROM tm_producto WHERE est = 1";
             $sql=$conectar->prepare($sql);
             $sql->execute();
-            return $resultado=$sql->fetchAll();
+            return $resultado = $sql->fetchAll();
         }
 
         public function get_produto_x_id($prod_id){
 
             //Creacion de variables que extienden desde la clase Conectar
             $conectar = parent::conexion();
-            parent::set_names();
+            //parent::set_names();
             $sql = "SELECT * FROM tm_producto WHERE prod_id = ?";
             $sql=$conectar->prepare($sql);
             $sql->bindValue(1, $prod_id);
@@ -30,7 +30,7 @@
 
             //Creacion de variables que extienden desde la clase Conectar
             $conectar = parent::conexion();
-            parent::set_names();
+            //parent::set_names();
             $sql = "UPDATE tm_producto SET est = 0, fech_elim = now() WHERE prod_id = ?";
             $sql=$conectar->prepare($sql);
             $sql->bindValue(1, $prod_id);
@@ -42,7 +42,7 @@
 
             //Creacion de variables que extienden desde la clase Conectar
             $conectar = parent::conexion();
-            parent::set_names();
+            //parent::set_names();
             $sql = "INSERT INTO tm_producto (prod_id, prod_nom, fech_crea, fech_mod, fech_elim, est)
                     VALUES 
                     (NULL, ? , now(), NULL, NULL, 1);";
@@ -56,7 +56,7 @@
 
             //Creacion de variables que extienden desde la clase Conectar
             $conectar = parent::conexion();
-            parent::set_names();
+            //parent::set_names();
             $sql = "UPDATE tm_producto SET prod_nom = ?, fech_mod = now() WHERE prod_id = ?";
             $sql=$conectar->prepare($sql);
             $sql->bindValue(1, $prod_nom);
