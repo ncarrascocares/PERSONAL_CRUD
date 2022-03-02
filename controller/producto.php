770c3cdfccbbe4f267bd6ee86ab592febec7b9c1
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2022-03-01 23:50:14 -0300

    Correción de los array

--- controller/producto.php
+++ controller/producto.php
@@ -6,24 +6,27 @@ require_once("../models/Producto.php");
 //instanciando un ubjeto de la clase Productos
 $producto = new Producto();
 
-switch ($_GET['op']) {
-    case 'listar':
-        $datos = $producto->get_produto();
+switch ($_GET["op"]) {
+    case "listar":
+        $datos = $producto->get_producto();
         $data = Array();
         foreach ($datos as $row) {
             $sub_array = array();
-            $sub_array[] = $row['prod_nom'];
-            $sub_array[] = '<button type="button" onClick="editar('.$row["prod_id"].');" id="'.$row["prod_id"].'" class="btn btn-outline-primary btn-icon"><div><i class="fa fa-edit"></i></div></button>';
-            $sub_array[] = '<button type="button" onClick="eliminar('.$row["prod_id"].');" id="'.$row["prod_id"].'" class="btn btn-outline-danger btn-icon"><div><i class="fa fa-trash"></i></div></button>';
+            $sub_array[] = $row["prod_nom"];
+            $sub_array[] = '<button type"button" onClick="ver(' . $row["prod_id"] . ');" id="' . $row["prod_id"] . '" class="btn btn-inline-primary btn-sm ladda-button"><i class="fa fa-eye"></i></div></button>';
+            $sub_array[] = '<button type"button" onClick="ver(' . $row["prod_id"] . ');" id="' . $row["prod_id"] . '" class="btn btn-inline-primary btn-sm ladda-button"><i class="fa fa-eye"></i></div></button>';
+
+            $data[] = $sub_array;
         }
 
         $results = array(
             "sEcho"=>1,
             "iTotalRecords"=>count($data),
             "iTotalDisplayRecords"=>count($data),
-            "aaData"=>$data);
+            "aaData"=>$data
+        );
 
-        echo json_encode($results);
+            echo json_encode($results);
 
     break;
 
