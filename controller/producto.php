1fa0c35698f2a6e25c84c335d68f44f3149ea1d0
nicolascarrasco15 <nicolascarrasco15@gmail.com>
2022-03-03 23:53:29 -0300

    03-03-2022 se agregan los case en el controlador guardaryeditar, mostrar y eliminar

--- controller/producto.php
+++ controller/producto.php
@@ -13,8 +13,8 @@ switch ($_GET["op"]) {
         foreach ($datos as $row) {
             $sub_array = array();
             $sub_array[] = $row["prod_nom"];
-            $sub_array[] = '<button type"button" onClick="ver(' . $row["prod_id"] . ');" id="' . $row["prod_id"] . '" class="btn btn-outline-primary btn-icon"><i class="fa fa-edit"></i></div></button>';
-            $sub_array[] = '<button type"button" onClick="ver(' . $row["prod_id"] . ');" id="' . $row["prod_id"] . '" class="btn btn-outline-danger btn-sicon"><i class="fa fa-trash"></i></div></button>';
+            $sub_array[] = '<button type"button" onClick="editar(' . $row["prod_id"] . ');" id="' . $row["prod_id"] . '" class="btn btn-outline-primary btn-icon"><i class="fa fa-edit"></i></div></button>';
+            $sub_array[] = '<button type"button" onClick="eliminar(' . $row["prod_id"] . ');" id="' . $row["prod_id"] . '" class="btn btn-outline-danger btn-sicon"><i class="fa fa-trash"></i></div></button>';
 
             $data[] = $sub_array;
         }
@@ -29,6 +29,49 @@ switch ($_GET["op"]) {
             echo json_encode($results);
 
     break;
+
+    case "guardaryeditar":
+        //como desde la vista se vana enviar datos a nuestro controlador, aca se recibiran a traves de un post
+        //el parametro que recibiremos es el post_id
+        $datos = $producto->get_produto_x_id($_POST['prod_id']);
+
+        //Luego preguntamos si prod_id esta vacio
+        if (empty($_POST['prod_id'])) {
+
+            //Acá hacemos otra validación adicional
+            //Si datos no trajo ningún registro o ningún array
+            if (is_array($datos) == true and count($datos) == 0) {
+                //acá se realiza un insert y se entrega los parametros necesarios, en este caso 1 
+                $producto->insert_produto($_POST['prod_nom']);
+            }else{
+                //caso contrario se realiza un editar y se entregan los parametros necesarios, en este caso necesita 2 para realizar la sentencia creada en el modelo
+                $producto->update_produto($_POST['prod_id'], $_POST['prod_nom']);
+            }
+        }
+
+    break;
+
+   
+    case "mostrar":
+     //¿Que va a realizar este case mostrar?
+        //Va a traer todos los datos y los va a separar en un json
+        $datos = $producto->get_produto_x_id($_POST['prod_id']);
+          //Si datos tiene información
+          if (is_array($datos) == true and count($datos) > 0) {
+            
+            //recorremos con un foreach los datos almacenados en la variable $datos
+            foreach ($datos as $row) {
+                $salida["prod_id"] = $row["prod_id"];
+                $salida["prod_nom"] = $row["prod_nom"];
+            }
+        }
+
+    break;
+
+    case "eliminar":
+        $producto->delete_produto($prod_id);
+    break;
+    
 
 }
 ?>
